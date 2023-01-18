2f390fb2833260871b7541330a5f9d44d0a9b9da
asadi18 <asadishteaque@gmail.com>
2023-01-18 07:47:18 +0600

    Updates on sticky navbar - improved reusability

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const navEl = document.querySelector(".navbar");
 const btEl = document.querySelector(".bottom-container");
 
-window.addEventListener("scroll", () => {
+const stickyNav = () => {
   if (window.scrollY > btEl.offsetTop - navEl.offsetHeight - 50) {
     navEl.classList.add("active");
   } else {
     navEl.classList.remove("active");
   }
-});
-console.log("Body = " + btEl.offsetTop, "navbar = " + window.scrollY);
+  console.log("Body = " + btEl.offsetTop, "navbar = " + window.scrollY); // Just for cheking the position
+};
+window.addEventListener("scroll", stickyNav);
